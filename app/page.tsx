5f250019ca2be04cd9e019405566a0dc0a861d9f
Asmita Mallick <asmitamallick2@gmail.com>
2025-01-23 16:46:30 +0530

    stats-section.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -101,12 +101,14 @@
 // }
 "use client";
 import InternshipForm from "@/app/components/internship-form";
-import Footer from "./components/footer";
+// import Footer from "./components/footer";
+// import StatsSection from "./components/stats-section";
 export default function Page() {
   return (
     <>
       <InternshipForm />
-      <Footer />
+      {/* <Footer />
+      <StatsSection /> */}
     </>
   );
 }
