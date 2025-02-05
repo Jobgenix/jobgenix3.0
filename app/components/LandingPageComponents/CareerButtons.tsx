ab4b727879dab19e3bcb553b3a74432e019595c8
Aditya <lateraladityaraj@gmail.com>
2025-02-05 22:32:10 +0530

    shadow added

--- app/components/LandingPageComponents/CareerButtons.tsx
+++ app/components/LandingPageComponents/CareerButtons.tsx
@@ -3,7 +3,7 @@ import { Briefcase, GraduationCap, FileUp } from "lucide-react"
 export default function CareerButtons() {
   return (
     <div className="flex flex-col w-full items-center gap-4 p-6  mx-auto md:hidden bg-gradient-to-r from-[#C6F7D5] to-[#FFFCEF] ">
-      <button className="group flex flex-col items-center p-4 bg-[#F9E39E] w-4/5 rounded-2xl hover:shadow-lg transition-all duration-300">
+      <button className="group shadow-lg shadow-gray-400 flex flex-col items-center p-4 bg-[#F9E39E] w-4/5 rounded-2xl hover:shadow-lg transition-all duration-300">
         <div className="flex items-center gap-2 mb-1">
           
           <span className="font-semibold text-lg">Jobs</span>
@@ -12,7 +12,7 @@ export default function CareerButtons() {
         <span className="text-sm text-gray-600">Find the right opportunity</span>
       </button>
 
-      <button className="group flex flex-col items-center p-4 bg-[#D5CDFD] w-4/5 rounded-2xl hover:shadow-lg transition-all duration-300">
+      <button className="group shadow-lg shadow-gray-400 flex flex-col items-center p-4 bg-[#D5CDFD] w-4/5 rounded-2xl hover:shadow-lg transition-all duration-300">
         <div className="flex items-center gap-2 mb-1">
           
           <span className="font-semibold text-lg">Internships</span>
@@ -21,7 +21,7 @@ export default function CareerButtons() {
         <span className="text-sm text-gray-600">Start Your Career</span>
       </button>
 
-      <button className="group flex flex-col items-center p-4 bg-[#EBCABFF7] w-4/5 rounded-2xl hover:shadow-lg transition-all duration-300">
+      <button className="group shadow-lg shadow-gray-400 flex flex-col items-center p-4 bg-[#EBCABFF7] w-4/5 rounded-2xl hover:shadow-lg transition-all duration-300">
         <div className="flex items-center gap-2 mb-1">
           
           <span className="font-semibold text-lg">Power up your career</span>
