9032afd6c75e508b88d39f08260b66f0c824a4b6
Jobgenix <teamjobgenix@gmail.com>
2025-03-15 22:23:07 +0530

    Delete app/api/extCv directory

--- app/api/extCv/route.ts
+++
@@ -1,7 +0,0 @@
-
-import { NextResponse } from "next/server";
-
-export async function POST(req : Request) {
-  console.log("Received request at /api/cvextract");
-  return NextResponse.json({ message: "API is working!" });
-}
