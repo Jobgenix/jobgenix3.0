87a424430108b92cdfaf7dbc37ed229381e51d06
Jobgenix <teamjobgenix@gmail.com>
2025-03-19 21:01:41 +0530

    Add Prof. Sec. on moob.

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -24,7 +24,6 @@ export default function ProfilePage() {
   if (!session?.user?.id) {
     return null; // Prevents errors when session is null
   }
-
   return (
     <div className="h-auto w-full bg-[#c6f7d5]">
       <Navbar />
