4e33e05fa616e799267237bffd95d98cf60982f8
Anirban Kundu <kunduani2003@gmail.com>
2025-04-06 09:47:31 +0530

    Chages

--- app/components/job-display-new/job-dis.tsx
+++ app/components/job-display-new/job-dis.tsx
@@ -1,4 +1,4 @@
-import { Search } from "lucide-react"
+// import { Search } from "lucide-react"
 import { Button } from "../ui/button"
 import { Input } from "../ui/input"
 import { Badge } from "../ui/badge"
