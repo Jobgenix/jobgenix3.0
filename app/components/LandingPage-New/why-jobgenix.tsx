4908e260ca17cb992d39557b9a86b24ba699cfd4
Anirban Kundu <kunduani2003@gmail.com>
2025-04-06 09:08:53 +0530

    Fixed Issues

--- app/components/LandingPage-New/why-jobgenix.tsx
+++ app/components/LandingPage-New/why-jobgenix.tsx
@@ -1,11 +1,9 @@
 "use client";
 
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
-import { FreeMode } from "swiper/modules";
+
 
 
 export default function Whyjobgenix() {
