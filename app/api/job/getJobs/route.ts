71f2390e07710c588220456ee5480dbef4666964
Anirban Kundu <kunduani2003@gmail.com>
2025-04-28 22:00:48 +0530

    Fix the route.ts issues

--- app/api/job/getJobs/route.ts
+++ app/api/job/getJobs/route.ts
@@ -236,7 +236,7 @@ async function getJobs(req: NextRequest) {
     const filters = [];
     if (name) filters.push(ilike(opportunities.title, `%${name}%`));
     if (lastJobId) filters.push(gt(opportunities.id, lastJobId));
-    if (stream) filters.push(sql`${stream} = ANY(${opportunities.degree})`);
+    if (stream) {filters.push(sql`${stream} = ANY(${opportunities.degree})`);}
     if (passingYear)
       filters.push(sql`${passingYear} = ANY(${opportunities.passoutYear})`);
     if (type) filters.push(eq(opportunities.type, type));
