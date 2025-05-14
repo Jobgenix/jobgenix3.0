1efbdfbcd0fe8f6d05b181ae8491f97087325ba8
Anirban Kundu <kunduani2003@gmail.com>
2025-05-14 22:34:33 +0530

    Done

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,16 +1,16 @@
 "use client";
 import React, { useState ,Suspense} from "react";
 import LoginModal from "../components/Modals/loginModal";
-import Hero from "../components/LandingPage-New/hero";
-import Nav from "../components/LandingPage-New/nav";
+// import Hero from "../components/LandingPage-New/hero";
+// import Nav from "../components/LandingPage-New/nav";
 
 export default function Page() {
   const [isLoginOpen, setIsLoginOpen] = useState(true); // Modal is open by default
 
   return (
     <div>
-      <Nav />
-      <Hero />
+      {/* <Nav />
+      <Hero /> */}
       {isLoginOpen && (
         <Suspense fallback={<div>Loading...</div>}>
         <LoginModal onClose={() => setIsLoginOpen(false)} /> {/* Pass onClose prop */}
